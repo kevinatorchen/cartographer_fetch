33b7851e068bc62520851a6b8c3863f17762451c
Kevin Chen <kevinjulianchen@hotmail.com>
2018-11-13 16:01:01 -0500

    Works for full robot simulator

--- cartographer_fetch/configuration_files/rail_fetch_2d.lua
+++ cartographer_fetch/configuration_files/rail_fetch_2d.lua
@@ -35,7 +35,7 @@ options = {
   submap_publish_period_sec = 0.3,
   pose_publish_period_sec = 5e-3,
   trajectory_publish_period_sec = 30e-3,
-  rangefinder_sampling_ratio = 0.1,
+  rangefinder_sampling_ratio = 1.,
   odometry_sampling_ratio = 1.,
   fixed_frame_pose_sampling_ratio = 1.,
   imu_sampling_ratio = 1.,
@@ -44,25 +44,28 @@ options = {
 
 MAP_BUILDER.use_trajectory_builder_2d = true
 
-TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 30
+TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1
 TRAJECTORY_BUILDER_2D.min_range = 0.
-TRAJECTORY_BUILDER_2D.max_range = 661.
+TRAJECTORY_BUILDER_2D.max_range = 20.
 TRAJECTORY_BUILDER_2D.missing_data_ray_length = 5.
-TRAJECTORY_BUILDER_2D.use_imu_data = false
+TRAJECTORY_BUILDER_2D.use_imu_data = false   -- Set this to true when running on actual Fetch robot (not simulator).
 
 POSE_GRAPH.constraint_builder.min_score = 0.65
 POSE_GRAPH.optimization_problem.huber_scale = 3
 
 POSE_GRAPH.optimization_problem.local_slam_pose_translation_weight = 1e3
 POSE_GRAPH.optimization_problem.local_slam_pose_rotation_weight = 1e2
-POSE_GRAPH.optimization_problem.odometry_translation_weight = 1e3
+POSE_GRAPH.optimization_problem.odometry_translation_weight = 	1e3
 POSE_GRAPH.optimization_problem.odometry_rotation_weight = 1e2
 
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.occupied_space_weight = 10
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 40
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.occupied_space_weight = 100
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 1e3
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 4e3
+TRAJECTORY_BUILDER_2D.submaps.num_range_data = 30
 
-TRAJECTORY_BUILDER_2D.submaps.num_range_data = 120
 TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.1
 TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.2)
+
+
 
 return options
